f44cfa1b4bfc8e3794bb183d52fc9b575e42b4bd
MohmadMedo <152075646+MohmadMedo@users.noreply.github.com>
2026-02-09 00:32:06 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php
-$age = 16; 
-
-if ($age >= 18) {
-    echo "welcome to the site";
-} else {
-    echo "not allowed to access the site";
+function calculate($num1, $num2): void {
+  
+    $multiplication = $num1 * $num2;
+    $difference = $num1 - $num2;
+    if ($num2 != 0) {
+        $division = $num1 / $num2;
+    } else {
+        $division = "Cannot divide by zero";
+    }
+    echo "Multiplication: " . $multiplication . "<br>";
+    echo "Difference: " . $difference . "<br>";
+    echo "Division: " . $division . "<br>";
 }
+calculate(num1: 10, num2: 5);
 ?>
